2fac2d90d2ae9935b33ae8086a7621a06ff595e4
RivaGiorgia <riva.giorgia@itisgrassi.edu.it>
2025-02-12 08:20:53 +0100

    corretto

--- Esercizio_1.java
+++ Esercizio_1.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
-public class Esercizio_1
-{
-	public static void main(String[] args) {
+public class Esercizio_1 {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        int[] numeri = new int[4];
-        for (int i = 0; i < 4; i++) {
-                System.out.println("Inserisci quattro numeri: ");
-                numeri[i]= scanner.nextInt();
-                }
+        System.out.println("Inserisci quattro numeri:");
 
-                scanner.close();
-	}
+        for (int i = 0; i < 4; i++) { 
+            int numero = scanner.nextInt();
+
+           
+            if (numero == 0) {
+                System.out.println("Programma terminato.");
+                break; 
+            }
+
+            paridispari(numero);
+        }
+
+        scanner.close();
+    }
+
+   
+
+    public static void paridispari(int numero) {
+        if (numero % 2 == 0) {
+            System.out.println(numero + " **"); 
+        } else if (numero % 3 == 0) {
+            System.out.println(numero + " ##"); 
+        } else {
+            System.out.println(numero + " numero non valido, continua"); 
+        }
+    }
 }
+
